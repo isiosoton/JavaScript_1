ce64d13f9ac4af00605f37a383696daf18ae2211
isiosoton <isiosoton@gmail.com>
2021-05-09 22:14:25 +0900

    0509a

--- api/index.js
+++ api/index.js
@@ -11,7 +11,9 @@ const accessToken = 'DpMVi4S6+PpktL5GhoppoXn76ERRf+Alfbz+Tl5MNb8dGJWB7ifs4fAE/n1
 app.post('/', function (req, res) {
   const messageId = req.body['events'][0]['message']['id'];
   const replyToken = req.body['events'][0]['replyToken'];
-  console.log(messageId);  
+  //console.log(req.body);
+  //console.log(messageId);
+  //console.log(replyToken);
   res.send('api: Hello World!');
 
   const options = {
@@ -44,39 +46,46 @@ app.post('/', function (req, res) {
     }
 
     //Costom VisionからVercel
-    request(option, function(error, res, body){
-      //以下記入
-      request.post(option, function(error,res,body){
-        const resBody = JSON.parse(body);
-        
-        const messageData = {
-          "replyToken": replyToken, /*replyToken*/
-          "messages":[
-            {
-              "type":"text",
-              "text":"Hello World"
-            }
-          ]
-        }
+    //以下記入
+    request.post(option, function(error,res,body){
+      const resBody = JSON.parse(body);
+      const tagName = resBody.predictions;
 
-        const optionsLine = {
-          uri:'https://api.line.me/v2/bot/message/reply',
-          //method: 'post',
-          headers: {
-            'Content-Type':'application/json',
-            'Authorization':'Bearer' + accessToken,
-          },
-          json: messageData
-        }
-
-        //VercelからLINE
-        request.post(optionsLine, function(error,res,body){
-          //const tagName;
-          //console.log(tagName);
-        });
+      tagName.sort(function(first,second){
+          return second.probability - first.probability;
       });
-      //console.log(body);
+
+      console.log(resBody.predictions[0]);
+ 
+      const messageData = {
+        "replyToken": replyToken,
+        "messages":[
+          {
+            "type":"text",
+            "text":"これは" + resBody["predictions"][0]["tagName"] + "です"
+          },
+          {
+            "type":"text",
+            "text":"確率は" + resBody["predictions"][0]["probability"] * 100　+"%です"
+          }
+        ]
+      }
+
+      const optionsLine = {
+        uri:'https://api.line.me/v2/bot/message/reply',
+        headers: {
+          'Content-Type':'application/json',
+          'Authorization':'Bearer ' + accessToken,
+        },
+        json: messageData
+      }
+
+      //VercelからLINE
+      request.post(optionsLine, function(error,res,body){
+        console.log(body);
+      });
     });
+    //console.log(body);
   });
 });
 
@@ -89,4 +98,24 @@ U4c345b6fb05bc1e9d895650be0c3b7fd
 
 チャンネルアクセストークン
 DpMVi4S6+PpktL5GhoppoXn76ERRf+Alfbz+Tl5MNb8dGJWB7ifs4fAE/n1Qksbuy9E4cjq4D9f6gI2w9bOyZ+53H7aHBxMdrVG5yTlm9a5iRiK6O5mX1xNbo2tR00VFhnx92fTpo0q+rPGUMGDDbAdB04t89/1O/w1cDnyilFU=
+
+JSON形式
+body = {
+    "id": "4c2050bd-1a8a-489c-b513-607a8cf87842",
+    "project": "827c13e4-0a7e-406d-9748-9c92e3b6ac3d",
+    "iteration": "2243c8b2-5cfe-49e5-b3a5-a8ff81280817",
+    "created": "2021-05-09T10:03:53.922Z",
+    "predictions": [
+        {
+            "probability": 0.8471966,
+            "tagId": "adfce210-86c5-46d4-88a0-0a5edd14c1d7",
+            "tagName": "mikan"
+        },
+        {
+            "probability": 0.14872465,
+            "tagId": "c36f7a3e-0f9f-418e-bc2d-b95ba2f276c6",
+            "tagName": "kabosu"
+        }
+    ]
+}
 */
